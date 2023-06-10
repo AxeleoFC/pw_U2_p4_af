8f428434e89cea809c0bb50d01c1c15411810480
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-10 10:47:20 -0500

    Taller 15

--- js/vFor.js
+++ js/vFor.js
@@ -1,6 +1,55 @@
+function estudiantes(){
+    var estu= []
+    for (let index = 0; index < 10; index++) {
+        const estudiante={
+            nomb: "Estudiante Nombre "+index,
+            apell: "Estudiante Apellido "+index
+        }
+        estu.push(estudiante)
+    }
+    return estu
+}
 
 const app = Vue.createApp({
-
+    data(){
+        return{
+            miarreglo:estudiantes(),
+            nombre:'Axel',
+            apellido:'Flores'
+        }
+    },
+    methods:{
+        agregarEstudiante(event){
+            console.log("Vamos a agregar sin modificador de evento")
+            console.log(this.nombre)
+            console.log(event.charCode)
+            if (event.charCode=='13') {
+                console.log('Es enter')
+                const nEstudiante={
+                    nomb: this.nombre,
+                    apell: this.apellido
+                }
+                this.miarreglo.unshift(nEstudiante)
+            }else{
+                console.log('No es enter')
+            }
+        },
+        agregarEstudiante2(){
+            console.log('Modificador de evento')
+            const nEstudiante={
+                nomb: this.nombre,
+                apell: this.apellido
+            }
+            this.miarreglo.unshift(nEstudiante)
+        }
+    }
 })
 
 app.mount('#myApp2')
+/*
+function insertar(estudiante) {
+    
+}
+function insertar({nombre,apellido}) {
+    
+}*/
